a42293087a4c8a832cc1ef00b8127ad1eae1dbe9
coolN1ckname <160507741+coolN1ckname@users.noreply.github.com>
2024-11-01 15:18:02 +0500

    added  "Отдать в приют" page

--- docs/script.js
+++ docs/script.js
@@ -11,3 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
         mobileMenu.classList.remove('open');
     });
 });
+function toggleAdditionalFields() {
+    const selectElement = document.getElementById('cat-breed');
+    const additionalFields = document.getElementById('additional-fields');
+    const sourceInput = document.getElementById('source');
+
+    if (selectElement.value === 'Домашний') {
+        additionalFields.style.display = 'block';
+        sourceInput.placeholder = 'Причина сдачи'; 
+    } else {
+        additionalFields.style.display = 'none';
+        sourceInput.placeholder = 'Где нашли'; 
+    }
+}
+
