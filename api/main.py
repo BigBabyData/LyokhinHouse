e5a0e01be405c743ddfa38bc8dff3f676da83ab5
mndtr <armagon052@gmail.com>
2024-11-10 15:20:56 +0500

    [test]

--- api/main.py
+++ api/main.py
@@ -1,13 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Test"
+def home():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    # app.run()
-
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
