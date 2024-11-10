04bd418f087ac86b02593dec4e52c222f060ed71
mndtr <armagon052@gmail.com>
2024-11-10 15:24:40 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, World!"
+def index():
+    return "Test"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    # app.run()
+
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
